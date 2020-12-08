8288519d098dfab286a0810a6d73707e09b63ee1
ihollander <ianholla@gmail.com>
2020-12-07 19:13:54 -0500

    Added solution

--- src/createStore.js
+++ src/createStore.js
@@ -1,28 +1,40 @@
-let state;
+function createStore(reducer) {
+  let state;
+
+  function dispatch(action) {
+    state = reducer(state, action);
+    render();
+  }
+
+  function getState() {
+    return state;
+  }
+
+  return {
+    dispatch,
+    getState,
+  };
+}
 
 function reducer(state = { count: 0 }, action) {
   switch (action.type) {
-    case 'INCREASE_COUNT':
+    case "counter/increment":
       return { count: state.count + 1 };
 
     default:
       return state;
   }
-};
-
-function dispatch(action){
-  state = reducer(state, action);
-  render();
-};
+}
 
 function render() {
-  let container = document.getElementById('container');
-  container.textContent = state.count;
-};
+  let container = document.getElementById("container");
+  container.textContent = store.getState().count;
+}
 
-dispatch({ type: '@@INIT' })
-let button = document.getElementById('button');
+let store = createStore(reducer); // createStore takes the reducer as an argument
+store.dispatch({ type: "@@INIT" });
+let button = document.getElementById("button");
 
-button.addEventListener('click', function() {
-    dispatch({ type: 'INCREASE_COUNT' });
-})
+button.addEventListener("click", () => {
+  store.dispatch({ type: "counter/increment" });
+});
